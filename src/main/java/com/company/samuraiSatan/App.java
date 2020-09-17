4715427620f5a1aa2a8a8edc9e4ca425291f6eda
maciek <mcyran1996@gmail.com>
2020-09-17 09:58:23 +0200

    work in progress5

--- src/main/java/com/company/samuraiSatan/App.java
+++ src/main/java/com/company/samuraiSatan/App.java
@@ -2,6 +2,7 @@ package com.company.samuraiSatan;
 
 import com.company.samuraiSatan.controller.CreepStartingPageHandler;
 import com.company.samuraiSatan.controller.LoginHandler;
+import com.company.samuraiSatan.controller.AddArtifactHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ public class App {
         server.createContext("/login", new LoginHandler());
         server.createContext("/static", new Static());
         server.createContext("/creepStarting", new CreepStartingPageHandler());
+        server.createContext("/mentorAddArtifact", new AddArtifactHandler());
 
         server.setExecutor(null);
         server.start();
--- src/main/java/com/company/samuraiSatan/controller/AddArtifactHandler.java
+++ src/main/java/com/company/samuraiSatan/controller/AddArtifactHandler.java
@@ -1,8 +1,8 @@
 package com.company.samuraiSatan.controller;
 
-import com.company.samuraiSatan.dao.UserDao;
+import com.company.samuraiSatan.dao.ArtifactDao;
 import com.company.samuraiSatan.helpers.Parser;
-import com.company.samuraiSatan.models.User;
+import com.company.samuraiSatan.models.Artifact;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -11,13 +11,14 @@ import java.io.*;
 import java.net.HttpCookie;
 import java.util.Map;
 
-public class LoginHandler implements HttpHandler {
+public class AddArtifactHandler implements HttpHandler {
 //    ReadHtml readHtml = new ReadHtml();
-    UserDao userDao = new UserDao();
+    ArtifactDao artifactDao = new ArtifactDao();
     private ObjectMapper objectMapper = new ObjectMapper();
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
         String response = null;
+
         try {
             String method = httpExchange.getRequestMethod();
             if(method.equals("POST")) {
@@ -25,14 +26,16 @@ public class LoginHandler implements HttpHandler {
                 BufferedReader br = new BufferedReader(isr);
 
                 Map<String, String> data = Parser.parseFormData(br.readLine());
-                String email = data.get("email");
-                String password = data.get("password");
-
-                User user = userDao.getUser(email, password);
-                String userJSON = objectMapper.writeValueAsString(user);
-                HttpCookie cookie = new HttpCookie("user", userJSON);
+                String artifactName = data.get("artifactName");
+                int codecoinsCost = Integer.parseInt(data.get("codecoinsCost"));
+                String artifactDescription = data.get("artifactDescription");
+                System.out.println(artifactName);
+                Artifact artifact = new Artifact(0,artifactName, codecoinsCost,true,artifactDescription,true);
+                artifactDao.addNewArtifact(artifact);
+                String artifactJSON = objectMapper.writeValueAsString(artifact);
+                HttpCookie cookie = new HttpCookie("artifact", artifactJSON);
                 httpExchange.getResponseHeaders().add("Set-Cookie", cookie.toString());
-                HttpCommunication.sendResponse(userJSON, httpExchange, 200);
+                HttpCommunication.sendResponse(artifactJSON, httpExchange, 200);
             }
         } catch (Exception e) {
             HttpCommunication.sendResponse(e.getMessage(), httpExchange, 404);
