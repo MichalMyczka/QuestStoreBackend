8cca3075a27cc9593ad358122adefa930334d93d
maciek <mcyran1996@gmail.com>
2020-09-29 14:38:35 +0200

    hotfix2

--- src/main/java/com/company/samuraiSatan/controller/LoginHandler.java
+++ src/main/java/com/company/samuraiSatan/controller/LoginHandler.java
@@ -7,8 +7,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+
 import java.io.*;
 import java.net.HttpCookie;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
 public class LoginHandler implements HttpHandler {
@@ -30,7 +33,33 @@ public class LoginHandler implements HttpHandler {
 
                 System.out.println(email);
 
-                User user = userDao.getUser(email, password);
+                String passwordToHash = password;
+                String generatedPassword = null;
+                try {
+                    // Create MessageDigest instance for MD5
+                    MessageDigest md = MessageDigest.getInstance("MD5");
+                    //Add password bytes to digest
+                    md.update(passwordToHash.getBytes());
+                    //Get the hash's bytes
+                    byte[] bytes = md.digest();
+                    //This bytes[] has bytes in decimal format;
+                    //Convert it to hexadecimal format
+                    StringBuilder sb = new StringBuilder();
+                    for(int i=0; i< bytes.length ;i++)
+                    {
+                        sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+                    }
+                    //Get complete hashed password in hex format
+                    generatedPassword = sb.toString();
+                }
+                catch (NoSuchAlgorithmException e)
+                {
+                    e.printStackTrace();
+                }
+                System.out.println(generatedPassword);
+
+
+                User user = userDao.getUser(email, generatedPassword);
                 String userJSON = objectMapper.writeValueAsString(user);
                 HttpCookie cookie = new HttpCookie("user", userJSON);
                 httpExchange.getResponseHeaders().add("Set-Cookie", cookie.toString());
